ca97f14f1cc50f93f3d6aa2508c3fecfb8ba46a3
[dyad] <nextcloud.forma@gmail.com>
2025-11-21 02:42:14 +0100

    [dyad] Added columns to steps table - wrote 1 file(s), executed 1 SQL queries

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -2,7 +2,10 @@ import { Database } from '../../database.types';
 
 export type Project = Database['public']['Tables']['projects']['Row'];
 export type Phase = Database['public']['Tables']['phases']['Row'];
-export type Step = Database['public']['Tables']['steps']['Row'];
+export type Step = Database['public']['Tables']['steps']['Row'] & {
+  guiding_questions?: string[] | null;
+  expected_output?: string | null;
+};
 export type Document = Database['public']['Tables']['documents']['Row'];
 export type DocumentVersion = Database['public']['Tables']['document_versions']['Row'];
 export type AiReview = Database['public']['Tables']['ai_reviews']['Row'];
